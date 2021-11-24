e5be4ce63dd77dd857eee30e10ba3d30d3b3aa7d
misha13123 <90206446+misha13123@users.noreply.github.com>
2021-11-24 13:27:32 +0200

    Add files via upload

---
+++ модуль17_1.ру.py
@@ -0,0 +1,11 @@
+num = int(input("введите целое число"))
+
+for x in range(10):
+    print(x, end=' ')
+lst=[]
+for item in range (10):
+    lst.append(item)
+print(lst)
+num1 = [0,3,2,6,67,4,8,9]
+num1.sort()
+print(num1)
